4192b23cb37c2487af555849811c41f62f541dd2
Rafael Alessandro <noprafa@gmail.com>
2020-10-26 10:42:20 -0300

    Resolvendo problemas com jQuery

--- home/views.py
+++ home/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, "home.html")
+
+
+def encurtador(request):
+    return render(request, "encurtador.html")
